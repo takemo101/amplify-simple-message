55e72348d0144a1bd17b212cfaeccebd53efa87e
takemo101 <takemo101@gmail.com>
2025-01-11 21:14:01 +0900

    [update] コメント追加

--- amplify/functions/message/handler.ts
+++ amplify/functions/message/handler.ts
@@ -3,6 +3,7 @@ import { createMessage } from './handlers/createMessage';
 import { getMessages } from './handlers/getMessages';
 import { removeMessage } from './handlers/removeMessage';
 
+// フィールド名（amplify data で定義したスキーマ名）と実行ハンドラーのマップ
 const handlers = {
   getMessages,
   createMessage,
@@ -41,10 +42,11 @@ export const handler: Handler = async (event, context, callback) => {
     throw new Error(`Invalid field: ${fieldName}`);
   }
 
+  // フィールド名に対応するハンドラーを取得して実行する
   const handler = handlers[fieldName];
 
   return await handler(
-    // @ts-ignore
+    // @ts-ignore 型がうまくいかなかったので回避
     event,
     context,
     callback,
